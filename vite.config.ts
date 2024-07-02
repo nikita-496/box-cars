6019a58ed4decd8911b858fe48bcd284707cf49c
nik <shevkunovnikita97@gmail.com>
2024-07-02 18:26:07 +0500

    chore: configure proxy rules for the dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+
+const env = loadEnv("all", process.cwd());
+const BASE_PUBLIC_API_URL = env["VITE_BASE_PUBLIC_API_URL"];
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +13,18 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
       "@assets": path.resolve(__dirname, "./src/assets"),
       "@components": path.resolve(__dirname, "./src/components"),
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@service": path.resolve(__dirname, "./src/service"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: BASE_PUBLIC_API_URL,
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 });
