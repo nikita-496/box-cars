2d7ed08ba4f3174e7227918387d5ec5abfda30f2
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2024-07-09 15:23:16 +0500

    feat: configure design tokens

---
+++ src/core/config/designTokens/font.ts
@@ -0,0 +1,24 @@
+export const font = {
+  "font-family": {
+    "sans-serif":
+      'system-ui,-apple-system,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
+    monospace:
+      'SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace',
+    "dm-sans": 'DM Sans", sans-serif',
+    "bebas-neue": '"Bebas Neue", sans-serif',
+  },
+  size: {
+    "x0.875": "x0.875rem",
+    "x0.9375": "0.9375rem",
+    base: "1rem",
+    "x1.125": "1.125rem",
+    "x1.25": "1.25rem",
+    "x1.375": "1.375rem",
+    "x1.5": "1.5rem",
+    "x1.875": "1.875rem",
+    "x2.5": "2.5rem",
+    "x2.375": "2.375rem",
+    "x4.25": "4.25rem",
+    "x4.375": "4.375rem",
+  },
+} as const;
