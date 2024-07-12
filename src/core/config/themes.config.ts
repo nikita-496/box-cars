7d603d47929dab2abe71c006c5261027e2b7ec33
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2024-07-12 14:27:14 +0500

    chore: setting fonts

--- src/core/config/themes.config.ts
+++ src/core/config/themes.config.ts
@@ -1,8 +1,30 @@
 import { color } from "@/core/config/designTokens/color";
+import { font } from "@/core/config/designTokens/font";
 import { Color, PaletteColor } from "@mui/material";
 import { createTheme, getContrastRatio } from "@mui/material/styles";
 
 declare module "@mui/material/styles" {
+  //eslint-disable-next-line
+  interface TypographyVariants {
+    "body_x0.875": React.CSSProperties;
+    "body_x0.875_m": React.CSSProperties;
+    "body_x0.9375_sm": React.CSSProperties;
+    "body_x0.9375_m": React.CSSProperties;
+    "body_x1.125": React.CSSProperties;
+    "body_x1.125_m": React.CSSProperties;
+    "h_x2.5": React.CSSProperties;
+  }
+  //eslint-disable-next-line
+  interface TypographyVariantsOptions {
+    "body_x0.875": React.CSSProperties;
+    "body_x0.875_m": React.CSSProperties;
+    "body_x0.9375_sm": React.CSSProperties;
+    "body_x0.9375_m": React.CSSProperties;
+    "body_x1.125": React.CSSProperties;
+    "body_x1.125_m": React.CSSProperties;
+    "h_x2.5": React.CSSProperties;
+  }
+
   //eslint-disable-next-line
   interface Palette {
     neutralLight: PaletteColor;
@@ -23,6 +45,19 @@ declare module "@mui/material/styles" {
   }
 }
 
+declare module "@mui/material/Typography" {
+  //eslint-disable-next-line
+  interface TypographyPropsVariantOverrides {
+    "body_x0.875": true;
+    "body_x0.875_m": true;
+    "body_x0.9375_sm": true;
+    "body_x0.9375_m": true;
+    "body_x1.125": true;
+    "body_x1.125_m": true;
+    "h_x2.5": true;
+  }
+}
+
 declare module "@mui/material/Button" {
   //eslint-disable-next-line
   interface ButtonPropsColorOverrides {
@@ -38,6 +73,34 @@ declare module "@mui/material/Button" {
 export let theme = createTheme({});
 
 theme = createTheme(theme, {
+  typography: {
+    "body_x0.875": {
+      fontSize: font.size["x0.875"],
+    },
+    "body_x0.875_m": {
+      fontSize: font.size["x0.875"],
+      lineHeight: "2",
+    },
+    "body_x0.9375_sm": {
+      fontSize: font.size["x0.9375"],
+      lineHeight: "1.74",
+    },
+    "body_x0.9375_m": {
+      fontSize: font.size["x0.9375"],
+      lineHeight: "1.87",
+    },
+    "body_x1.125": {
+      fontSize: font.size["x1.125"],
+    },
+    "body_x1.125_m": {
+      fontSize: font.size["x1.125"],
+      fontWeight: 500,
+      lineHeight: "1.333",
+    },
+    "h_x2.5": {
+      fontSize: font.size["x2.5"],
+    },
+  },
   palette: {
     common: {
       black: color.neutral[900],
@@ -86,12 +149,20 @@ theme = createTheme(theme, {
       },
     }),
   },
+
   components: {
     MuiButton: {
       styleOverrides: {
         root: () => ({
           fontSize: "1rem",
           textTransform: "none",
+        }),
+      },
+    },
+    MuiTypography: {
+      styleOverrides: {
+        root: () => ({
+          fontFamily: font.family["dm-sans"],
         }),
       },
     },
