e2c013050f573e140308199f1b95132c78fdb41c
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2024-07-19 15:20:35 +0500

    feat: state of base nav link

--- src/components/base/navLink/NavLink.tsx
+++ src/components/base/navLink/NavLink.tsx
@@ -1,15 +1,35 @@
+import { useTheme } from "@mui/material";
 import { FC } from "react";
 import {
   NavLink as ReactRouterNavLink,
   NavLinkProps as ReactRouterNavLinkProps,
 } from "react-router-dom";
 
-export type NavLinkProps = ReactRouterNavLinkProps;
+export type NavLinkProps = {
+  color?: string;
+  activeColor?: string;
+  activeBackground?: string;
+} & ReactRouterNavLinkProps;
 
-export const NavLink: FC<NavLinkProps> = ({ style, ...rest }) => {
+export const NavLink: FC<NavLinkProps> = ({
+  color,
+  activeColor,
+  activeBackground,
+  style,
+  ...rest
+}) => {
+  const theme = useTheme();
+  const themeColor = color || theme.palette.common.black;
+  const themeActiveColor = activeColor || theme.palette.primary.main;
+  const themeActiveBackground = activeBackground || '"transparent"';
   return (
     <ReactRouterNavLink
-      style={{ textDecoration: "none", ...style }}
+      style={({ isActive }) => ({
+        color: isActive ? themeActiveColor : themeColor,
+        backgroundColor: isActive ? themeActiveBackground : "transparent",
+        textDecoration: "none",
+        ...style,
+      })}
       {...rest}
     />
   );
