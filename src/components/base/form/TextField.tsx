f41652d3afb094b4f97a2c5731c09507d4414df6
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2024-07-23 14:07:48 +0500

    upd: add style text field of base

--- src/components/base/form/TextField.tsx
+++ src/components/base/form/TextField.tsx
@@ -1,22 +1,40 @@
 import {
   TextField as MUITextField,
   TextFieldProps as MUITextFieldProps,
+  styled,
   useTheme,
 } from "@mui/material";
 import { FC } from "react";
 
 export type TextFieldProps = MUITextFieldProps;
 
+const TextFieldStyled = styled(MUITextField)(({ theme }) => ({
+  "& .MuiInputBase-root": {
+    fontSize: theme.typography["body_x0.9375_m"],
+    fontFamily: '"DM Sans", sans-serif',
+    "&:before, &:after, &:hover:not(Mui-disabled, .Mui-error):before": {
+      border: "none",
+    },
+  },
+  "& .MuiInputLabel-standard": {
+    color: theme.palette.grey[800],
+    left: "1em",
+    top: "1.7em",
+    "&.Mui-focused": {
+      color: theme.palette.common.black,
+    },
+  },
+}));
+
 export const TextField: FC<TextFieldProps> = ({
   placeholder = "type...",
   label = "Input Text",
   variant = "standard",
-  sx,
   ...rest
 }) => {
   const theme = useTheme();
   return (
-    <MUITextField
+    <TextFieldStyled
       placeholder={placeholder}
       label={label}
       variant={variant}
@@ -33,25 +51,7 @@ export const TextField: FC<TextFieldProps> = ({
           },
         },
       }}
-      sx={{
-        "& .MuiInputBase-root": {
-          fontSize: theme.typography["body_x0.9375_m"],
-          fontFamily: '"DM Sans", sans-serif',
-          "&:before, &:after, &:hover:not(Mui-disabled, .Mui-error):before": {
-            border: "none",
-          },
-        },
-        "& .MuiInputLabel-standard": {
-          color: theme.palette.grey[800],
-          left: "1em",
-          top: "1.7em",
-          "&.Mui-focused": {
-            color: "common.black",
-          },
-        },
-        ...sx,
-      }}
       {...rest}
-    ></MUITextField>
+    />
   );
 };
