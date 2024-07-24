140a101520906c5434c1923017c3105f515cf3f8
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2024-07-24 13:22:20 +0500

    feat: add style of menu props

--- src/components/base/form/Select.tsx
+++ src/components/base/form/Select.tsx
@@ -1,7 +1,8 @@
 import {
   Select as MUISelect,
-  SelectProps,
+  SelectProps as MUISelectProps,
   OutlinedInput,
+  SxProps,
   useTheme,
 } from "@mui/material";
 
@@ -19,10 +20,16 @@ const menuProps = {
   },
 };
 
+export type SelectProps<T> = {
+  styleMenuProps?: SxProps;
+} & MUISelectProps<T>;
+
 export function Select<T>({
   label,
   IconComponent = ArrowDown,
   MenuProps,
+  styleMenuProps,
+  sx,
   ...rest
 }: SelectProps<T>) {
   const theme = useTheme();
@@ -42,8 +49,6 @@ export function Select<T>({
           },
           ".MuiMenu-list": {
             padding: "1.25em",
-            width: "auto !important",
-            paddingRight: "1.25em !important",
           },
           ".MuiMenuItem-root": {
             padding: "0 0.95em",
@@ -59,109 +64,38 @@ export function Select<T>({
           ".MuiMenuItem-root.Mui-selected .MuiTypography-root": {
             color: `${theme.palette.primary.main}`,
           },
+          ...styleMenuProps,
         },
         ...menuProps,
         ...MenuProps,
       }}
       sx={{
-        color: theme.palette.common.black,
         ".MuiSelect-select": {
           paddingTop: "1.9em",
           paddingBottom: "0.25em",
           fontSize: theme.typography["body_x0.9375_m"],
           fontFamily: '"DM Sans", sans-serif',
+          color: theme.palette.common.black,
         },
+
         "& .MuiOutlinedInput-notchedOutline": {
           borderRadius: 3,
           borderWidth: "2px",
         },
-        "&:hover .MuiOutlinedInput-notchedOutline": {
-          borderColor: theme.palette.common.black,
-        },
-        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-          borderColor: theme.palette.common.black,
-        },
+        "&:hover .MuiOutlinedInput-notchedOutline, &.Mui-focused .MuiOutlinedInput-notchedOutline":
+          {
+            borderColor: theme.palette.common.black,
+          },
+
         "& .MuiSelect-nativeInput": {
           height: "1px",
         },
         "& .MuiSvgIcon-root ": {
           transform: "translateX(-10px)",
         },
+        ...sx,
       }}
       {...rest}
     />
   );
 }
-
-/*export const Select = ({
-  label,
-  IconComponent = ArrowDown,
-  MenuProps,
-  ...rest
-}: ISelectProps<T>) => {
-  const theme = useTheme();
-  return (
-    <MUISelect
-      variant="outlined"
-      IconComponent={IconComponent}
-      input={<OutlinedInput notched name={`${label}`} />}
-      label={label}
-      MenuProps={{
-        sx: {
-          ".MuiMenu-paper": {
-            boxShadow: "none",
-            border: `2px solid ${theme.palette.common.black}`,
-            borderRadius: 3,
-          },
-          ".MuiMenu-list": {
-            padding: "1.25em",
-            width: "auto !important",
-            paddingRight: "1.25em !important",
-          },
-          ".MuiMenuItem-root": {
-            padding: "0 0.95em",
-            lineHeight: 2.5,
-          },
-          ".MuiMenuItem-root:hover": {
-            bgcolor: "transparent",
-          },
-          ".MuiMenuItem-root.Mui-selected": {
-            bgcolor: theme.palette.grey[300],
-            borderRadius: 3,
-          },
-          ".MuiMenuItem-root.Mui-selected .MuiTypography-root": {
-            color: `${theme.palette.primary.main}`,
-          },
-        },
-        ...menuProps,
-        ...MenuProps,
-      }}
-      sx={{
-        color: theme.palette.common.black,
-        ".MuiSelect-select": {
-          paddingTop: "1.9em",
-          paddingBottom: "0.25em",
-          fontSize: theme.typography["body_x0.9375_m"],
-          fontFamily: '"DM Sans", sans-serif',
-        },
-        "& .MuiOutlinedInput-notchedOutline": {
-          borderRadius: 3,
-          borderWidth: "2px",
-        },
-        "&:hover .MuiOutlinedInput-notchedOutline": {
-          borderColor: theme.palette.common.black,
-        },
-        "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-          borderColor: theme.palette.common.black,
-        },
-        "& .MuiSelect-nativeInput": {
-          height: "1px",
-        },
-        "& .MuiSvgIcon-root ": {
-          transform: "translateX(-10px)",
-        },
-      }}
-      {...rest}
-    />
-  );
-};*/
