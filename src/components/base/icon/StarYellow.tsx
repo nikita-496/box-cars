63a580dc1218f6e159709ba5809facf64e439b20
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2024-07-27 08:16:52 +0500

    feat: add icon wrapper and new icons

---
+++ src/components/base/icon/StarYellow.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { IconContext } from "react-icons";
+import { FaRegStar } from "react-icons/fa6";
+
+export const StarYellow: FC = () => {
+  return (
+    <IconContext.Provider value={{ color: "gold", size: "0.9375rem" }}>
+      <div>
+        <FaRegStar />
+      </div>
+    </IconContext.Provider>
+  );
+};
