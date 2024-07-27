2e3d871242d8a4bede75b636bb39f5f53e5ca029
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2024-07-27 08:24:33 +0500

    upd: props of design system

--- src/components/designSystem/card/Card.tsx
+++ src/components/designSystem/card/Card.tsx
@@ -1,20 +1,23 @@
+import { SxProps } from "@mui/system";
 import React, { FC } from "react";
 
 import { CardWrapper } from "@/components/base/elements";
 
-type CardProps = {
+export type CardProps = {
   cardToolbarSlot?: React.ReactNode;
   cardMediaSlot?: React.ReactNode;
   cardContentSlot?: React.ReactNode;
+  sx?: SxProps;
 };
 
 export const Card: FC<CardProps> = ({
   cardToolbarSlot,
   cardMediaSlot,
   cardContentSlot,
+  sx,
 }) => {
   return (
-    <CardWrapper sx={{ maxWidth: 345, position: "relative" }}>
+    <CardWrapper sx={{ maxWidth: 345, position: "relative", ...sx }}>
       {cardToolbarSlot}
       {cardMediaSlot}
       {cardContentSlot}
