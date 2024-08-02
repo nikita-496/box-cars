35601ffdd5b6d637185d65822012cd5684debd87
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2024-08-02 12:36:45 +0500

    feat: configure react-query

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { theme } from "@/core/config/themes.config";
 import { store } from "@/core/store";
 import { router } from "@/routes";
 import { ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
@@ -9,12 +11,27 @@ import { RouterProvider } from "react-router-dom";
 
 import "./index.css";
 
+export const queryClient = new QueryClient({
+  /**
+   * for development because of the limit on requests
+   */
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      enabled: false,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <RouterProvider router={router}></RouterProvider>
-      </ThemeProvider>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <RouterProvider router={router}></RouterProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </ThemeProvider>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
